2f7dcd88e9c49f2161095b47daa7549113394e31
MatheusCod <41351032+MatheusCod@users.noreply.github.com>
2020-09-18 21:15:27 -0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -55,7 +55,7 @@ cp Dockerfile $MATCHBOX
 
 cd $CONTAINERD
 printf "\nRUN yum -y install $CONTAINERD" >> Dockerfile
-printf "\nRUN $CONTAINERD aaaaaaaa || printf '$CONTAINERD error RPM AAAAAAAAAAAAAAAAAAAAAAA' >> log_error" >> Dockerfile
+printf "\nRUN $CONTAINERD -ajosjdiasd || printf '$CONTAINERD error RPM AAAAAAAAAAAAAAAAAAAAAAA' >> log_error" >> Dockerfile
 docker build -t $CONTAINERD-test -f $TRAVIS_BUILD_DIR/$CONTAINERD/Dockerfile .
 docker run -d $CONTAINERD-test
 docker cp $CONTAINERD-test:/log_error $TRAVIS_BUILD_DIR
