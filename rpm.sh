8ccb339d432de3d415c84d438101a226f90feb18
MatheusCod <41351032+MatheusCod@users.noreply.github.com>
2020-09-22 13:04:44 -0300

    Update rpm.sh

--- rpm.sh
+++ rpm.sh
@@ -59,7 +59,7 @@ cp Dockerfile $MATCHBOX
 
 cd $KIALI
 printf "\nRUN yum -y install $KIALI" >> Dockerfile
-printf "\nRUN $KIALI version" >> Dockerfile
+printf "\nRUN $KIALI -config \"version\" >> Dockerfile
 #printf "\nRUN if \[ \$\? \=\= 2 \]\; then exit 0\; else exit 1\; fi" >> Dockerfile
 {
   docker build -t $KIALI-test -f $LOCALPATH/$KIALI/Dockerfile .
