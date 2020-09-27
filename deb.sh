541802aec59ae43d8151a577ad4663def4120237
MatheusCod <41351032+MatheusCod@users.noreply.github.com>
2020-09-27 14:38:48 -0300

    Update deb.sh

--- deb.sh
+++ deb.sh
@@ -62,7 +62,7 @@ printf "\nRUN apt -y install docker-ce" >> Dockerfile
 printf "\nRUN apt -y install sudo" >> Dockerfile
 printf "\nRUN apt -y install conntrack" >> Dockerfile
 printf "\nRUN apt-get -y install $MINIKUBE\nRUN $MINIKUBE version" >> Dockerfile
-printf "\nRUN minikube start --driver=docker" >> Dockerfile
+printf "\nRUN minikube start --driver=none  --memory \"2048\" --cpus 2" >> Dockerfile
 {
   docker build -t $MINIKUBE-test -f $LOCALPATH/$MINIKUBE/Dockerfile .
 } || {
