be9e28c7e7428ffa3f3e7fe88af40038486e5414
MatheusCod <41351032+MatheusCod@users.noreply.github.com>
2020-09-30 19:07:29 -0300

    Update deb.sh

--- deb.sh
+++ deb.sh
@@ -59,11 +59,11 @@ cp Dockerfile $RCLONE
 cp Dockerfile $MATCHBOX
 
 cd $MINIKUBE
-printf "\nRUN apt -y install docker" >> Dockerfile
+printf "\nRUN apt -y install docker-ce" >> Dockerfile
 printf "\nRUN apt -y install sudo" >> Dockerfile
 printf "\nRUN apt -y install conntrack" >> Dockerfile
 printf "\nRUN apt-get -y install $MINIKUBE\nRUN $MINIKUBE version" >> Dockerfile
-printf "\nRUN minikube start --driver=none  --memory \"2048\" --cpus 2" >> Dockerfile
+printf "\nRUN minikube start --driver=docker  --memory \"2048\" --cpus 2" >> Dockerfile
 {
   docker build -t $MINIKUBE-test -f $LOCALPATH/$MINIKUBE/Dockerfile .
 } || {
