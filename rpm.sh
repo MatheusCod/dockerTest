6be29f8d45fff110f0ba68893e418993205ab65c
MatheusCod <41351032+MatheusCod@users.noreply.github.com>
2020-09-30 18:57:28 -0300

    Update rpm.sh

--- rpm.sh
+++ rpm.sh
@@ -57,6 +57,97 @@ cp Dockerfile $TERRAFORM
 cp Dockerfile $RCLONE
 cp Dockerfile $MATCHBOX
 
+cd $RESTIC
+printf "\nRUN yum -y install $RESTIC\nRUN $RESTIC version" >> Dockerfile
+#printf "\nRUN yes | $RESTIC -r restic-repo init" >> Dockerfile
+#printf "\nRUN yes | $RESTIC -r restic-repo backup ." >> Dockerfile
+#printf "\nRUN yes | $RESTIC -r restic-repo snapshots" >> Dockerfile
+{
+  docker build -t $RESTIC-test -f $LOCALPATH/$RESTIC/Dockerfile .
+} || {
+  printf "Error in RPM package, docker build process: $RESTIC\n" >> $TRAVIS_BUILD_DIR/log_error
+}
+{
+  docker run -d $RESTIC-test
+} || {
+  printf "Error in RPM package, docker run process: $RESTIC\n" >> $TRAVIS_BUILD_DIR/log_error
+}
+cd $LOCALPATH
+
+cd $GLIDE
+rm Dockerfile
+printf "FROM centos:8" >> Dockerfile
+printf "\nENV container docker" >> Dockerfile
+printf "\nRUN (cd /lib/systemd/system/sysinit.target.wants/; for i in *; do [ \$i == \\" >> Dockerfile
+printf "\nsystemd-tmpfiles-setup.service ] || rm -f \$i; done); \\" >> Dockerfile
+printf "\nrm -f /lib/systemd/system/multi-user.target.wants/*;\\" >> Dockerfile
+printf "\nrm -f /etc/systemd/system/*.wants/*;\\" >> Dockerfile
+printf "\nrm -f /lib/systemd/system/local-fs.target.wants/*; \\" >> Dockerfile
+printf "\nrm -f /lib/systemd/system/sockets.target.wants/*udev*; \\" >> Dockerfile
+printf "\nrm -f /lib/systemd/system/sockets.target.wants/*initctl*; \\" >> Dockerfile
+printf "\nrm -f /lib/systemd/system/basic.target.wants/*;\\" >> Dockerfile
+printf "\nrm -f /lib/systemd/system/anaconda.target.wants/*;" >> Dockerfile
+printf "\nVOLUME [ \"/sys/fs/cgroup\" ]" >> Dockerfile
+printf "\nCMD [\"/usr/sbin/init\"]" >> Dockerfile
+#printf "\nFROM golang:1.14" >> Dockerfile
+#printf "\nWORKDIR /go/src/app\n" >> Dockerfile
+printf "\nRUN printf \"[Open-Power]" >> Dockerfile
+printf "\\" >> Dockerfile
+printf "n" >> Dockerfile
+printf "name=Unicamp OpenPower Lab - \$basearch" >> Dockerfile
+printf "\\" >> Dockerfile
+printf "n" >> Dockerfile
+printf "baseurl=https://oplab9.parqtec.unicamp.br/pub/repository/rpm/" >> Dockerfile
+printf "\\" >> Dockerfile
+printf "n" >> Dockerfile
+printf "enabled=1" >> Dockerfile
+printf "\\" >> Dockerfile
+printf "n" >> Dockerfile
+printf "gpgcheck=0" >> Dockerfile
+printf "\\" >> Dockerfile
+printf "n" >> Dockerfile
+printf "repo_gpgcheck=1" >> Dockerfile
+printf "\\" >> Dockerfile
+printf "n" >> Dockerfile
+printf "gpgkey=https://oplab9.parqtec.unicamp.br/pub/key/openpower-gpgkey-public.asc\" >> /etc/yum.repos.d/open-power.repo" >> Dockerfile
+
+cd $MINIKUBE
+printf "\nRUN yum -y install docker" >> Dockerfile
+printf "\nRUN yum -y install sudo" >> Dockerfile
+printf "\nRUN yum -y install conntrack" >> Dockerfile
+printf "\nRUN yum -y install $MINIKUBE\nRUN $MINIKUBE version" >> Dockerfile
+printf "\nRUN $MINIKUBE start --driver=virtualbox  --memory \"2048\" --cpus 2" >> Dockerfile
+{
+  docker build -t $MINIKUBE-test -f $LOCALPATH/$MINIKUBE/Dockerfile .
+} || {
+  printf "Error in RPM package, docker build process: $MINIKUBE\n" >> $TRAVIS_BUILD_DIR/log_error
+}
+{
+  docker run -d $MINIKUBE-test
+} || {
+  printf "Error in RPM package, docker run process: $MINIKUBE\n" >> $TRAVIS_BUILD_DIR/log_error
+}
+cd $LOCALPATH
+
+cd $MINIO_MC
+MINIO_MC_PACKAGE="mc"
+printf "\nRUN timeout --preserve-status 5 mc" >> Dockerfile
+printf "\nRUN yum -y install $MINIO_MC_PACKAGE\nRUN $MINIO_MC_PACKAGE --version" >> Dockerfile
+{
+  docker build -t $MINIO_MC-test -f $LOCALPATH/$MINIO_MC/Dockerfile .
+} || {
+  printf "Error in RPM package, docker build process: $MINIO_MC\n" >> $TRAVIS_BUILD_DIR/log_error
+}
+{
+  docker run -d $MINIO_MC-test
+} || {
+  printf "Error in RPM package, docker run process: $MINIO_MC\n" >> $TRAVIS_BUILD_DIR/log_error
+}
+cd $LOCALPATH
+
+
+<< 'END'
+
 cd $CONTAINERD
 printf "\nRUN yum -y install $CONTAINERD\nRUN $CONTAINERD --version" >> Dockerfile
 {
